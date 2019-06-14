5265860d5c939d67457e3543b50c8f92736d7861
Park Je-Eon <ggungtoo@outlook.com>
2019-06-15 00:15:07 +0900

    asdf

--- LOMG/InGame.Designer.cs
+++ LOMG/InGame.Designer.cs
@@ -34,12 +34,13 @@
             this.timer_moveAndjump = new System.Windows.Forms.Timer(this.components);
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.WorldFrame = new System.Windows.Forms.Panel();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.labelServerIP = new System.Windows.Forms.Label();
+            this.labelClient = new System.Windows.Forms.Label();
             this.RightCharacter = new System.Windows.Forms.PictureBox();
             this.timer_gravity = new System.Windows.Forms.Timer(this.components);
-            this.labelClient = new System.Windows.Forms.Label();
-            this.labelServerIP = new System.Windows.Forms.Label();
-            this.textBoxIP = new System.Windows.Forms.TextBox();
-            this.buttonConnect = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.LeftCharacter)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.WorldFrame.SuspendLayout();
@@ -76,6 +77,7 @@
             // WorldFrame
             // 
             this.WorldFrame.BackColor = System.Drawing.Color.SkyBlue;
+            this.WorldFrame.Controls.Add(this.button1);
             this.WorldFrame.Controls.Add(this.buttonConnect);
             this.WorldFrame.Controls.Add(this.textBoxIP);
             this.WorldFrame.Controls.Add(this.labelServerIP);
@@ -86,6 +88,46 @@
             this.WorldFrame.Name = "WorldFrame";
             this.WorldFrame.Size = new System.Drawing.Size(1404, 381);
             this.WorldFrame.TabIndex = 4;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(797, 131);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 5;
+            this.buttonConnect.Text = "연결";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Visible = false;
+            this.buttonConnect.Click += new System.EventHandler(this.ButtonConnect_Click);
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(617, 133);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(174, 21);
+            this.textBoxIP.TabIndex = 4;
+            this.textBoxIP.Visible = false;
+            // 
+            // labelServerIP
+            // 
+            this.labelServerIP.AutoSize = true;
+            this.labelServerIP.Location = new System.Drawing.Point(547, 138);
+            this.labelServerIP.Name = "labelServerIP";
+            this.labelServerIP.Size = new System.Drawing.Size(69, 12);
+            this.labelServerIP.TabIndex = 3;
+            this.labelServerIP.Text = "서버 주소 : ";
+            this.labelServerIP.Visible = false;
+            // 
+            // labelClient
+            // 
+            this.labelClient.AutoSize = true;
+            this.labelClient.Font = new System.Drawing.Font("굴림", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelClient.Location = new System.Drawing.Point(632, 56);
+            this.labelClient.Name = "labelClient";
+            this.labelClient.Size = new System.Drawing.Size(147, 15);
+            this.labelClient.TabIndex = 2;
+            this.labelClient.Text = "클라이언트 대기중...";
+            this.labelClient.Visible = false;
             // 
             // RightCharacter
             // 
@@ -104,45 +146,15 @@
             this.timer_gravity.Interval = 1;
             this.timer_gravity.Tick += new System.EventHandler(this.Timer_gravity_Tick);
             // 
-            // labelClient
+            // button1
             // 
-            this.labelClient.AutoSize = true;
-            this.labelClient.Font = new System.Drawing.Font("굴림", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.labelClient.Location = new System.Drawing.Point(632, 56);
-            this.labelClient.Name = "labelClient";
-            this.labelClient.Size = new System.Drawing.Size(147, 15);
-            this.labelClient.TabIndex = 2;
-            this.labelClient.Text = "클라이언트 대기중...";
-            this.labelClient.Visible = false;
-            // 
-            // labelServerIP
-            // 
-            this.labelServerIP.AutoSize = true;
-            this.labelServerIP.Location = new System.Drawing.Point(547, 138);
-            this.labelServerIP.Name = "labelServerIP";
-            this.labelServerIP.Size = new System.Drawing.Size(69, 12);
-            this.labelServerIP.TabIndex = 3;
-            this.labelServerIP.Text = "서버 주소 : ";
-            this.labelServerIP.Visible = false;
-            // 
-            // textBoxIP
-            // 
-            this.textBoxIP.Location = new System.Drawing.Point(617, 133);
-            this.textBoxIP.Name = "textBoxIP";
-            this.textBoxIP.Size = new System.Drawing.Size(174, 21);
-            this.textBoxIP.TabIndex = 4;
-            this.textBoxIP.Visible = false;
-            // 
-            // buttonConnect
-            // 
-            this.buttonConnect.Location = new System.Drawing.Point(797, 131);
-            this.buttonConnect.Name = "buttonConnect";
-            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
-            this.buttonConnect.TabIndex = 5;
-            this.buttonConnect.Text = "연결";
-            this.buttonConnect.UseVisualStyleBackColor = true;
-            this.buttonConnect.Visible = false;
-            this.buttonConnect.Click += new System.EventHandler(this.ButtonConnect_Click);
+            this.button1.Location = new System.Drawing.Point(797, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "연결";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // InGame
             // 
@@ -180,6 +192,7 @@
         private System.Windows.Forms.TextBox textBoxIP;
         private System.Windows.Forms.Label labelServerIP;
         private System.Windows.Forms.Label labelClient;
+        private System.Windows.Forms.Button button1;
     }
 }
 
